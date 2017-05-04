36aba093dcc20259af3e12c83d86d364081386e1
Fabian <bafain@gmail.com>
2017-05-04 18:51:50 +0200

    markup

--- exercises/7/ex7.v
+++ exercises/7/ex7.v
@@ -2,13 +2,13 @@ Load "ex6.v".
 
 (** * Norm: Normalization of STLC *)
 
-(* Based on a chapter written and maintained by Andrew Tolmach *)
+(** _Based on a chapter written and maintained by Andrew Tolmach_. *)
 (* I recommend not looking at the chapter in SF, as it gives the whole
    proof away. *)
 
-Definition halts (t:tm) : Prop :=  exists t', t ⇒* t' /\ value t'.
+Definition halts (t:tm) : Prop := exists t', t ⇒* t' /\ value t'.
 
-(* The logical relation we will use.
+(** The logical relation we will use.
    Check Chapter 12 of Types and Programming Languages (Pierce)
 *)
 Fixpoint R (T:ty) (t:tm) : Prop :=
@@ -30,37 +30,37 @@ Lemma R_typable_empty : forall {T} {t}, R T t -> empty ⊢ t ∈ T.
   all: simpl in H; tauto.
 Defined.
 
-(* Exercise 1 (warm up): Show that the reduction relation is deterministic.
+(** _Exercise 1_ (warm up): Show that the reduction relation is deterministic.
    This is a very strong property; one may want to do the proof with a non-deterministic
    reduction relation. *)
-Lemma red_deterministic : forall t a b  (r : t ⇒ a) (s :  t ⇒ b), a = b.
+Lemma red_deterministic : forall t a b (r : t ⇒ a) (s : t ⇒ b), a = b.
 Admitted.
 
-(* Exercise 2: Show that R is preserved by the reduction relation, back and forth: *)
+(** _Exercise 2_: Show that [R] is preserved by the reduction relation, back and forth: *)
 Lemma step_preserves_R : forall T t t', (t ⇒ t') -> R T t -> R T t'.
 Admitted.
 
-Lemma step_preserves_R' : forall T t t', empty ⊢ t ∈ T ->  (t ⇒ t') -> R T t' -> R T t.
+Lemma step_preserves_R' : forall T t t', empty ⊢ t ∈ T -> (t ⇒ t') -> R T t' -> R T t.
 Admitted.
 
-(* Exercise 3: *)
+(** _Exercise 3_: *)
 
-(* We define the analogous of `R` for substitutions: *)
+(** We define the analogous of [R] for substitutions: *)
 Definition Rsubst Γ σ : Prop := has_types empty σ Γ /\ forall x T, Γ x = Some T -> R T (σ x).
 
-(* Show that R holds for all closed terms. 
-   For the induction to work, we need to consider a stronger property:  *)
+(** Show that [R] holds for all closed terms.
+   For the induction to work, we need to consider a stronger property: *)
 Lemma subst_R : forall Γ t T σ,
     Rsubst Γ σ ->
     Γ ⊢ t ∈ T ->
     R T (t.[σ]).
 Admitted.
 
-(* Then, proving closed_R is easy. *)
-Lemma closed_R : forall t T,  empty ⊢ t ∈ T -> R T t.
+(** Then, proving [closed_R] is easy. *)
+Lemma closed_R : forall t T, empty ⊢ t ∈ T -> R T t.
 Admitted.
 
-(* Exercise 4: Prove that the STλC is strongly normalizing.
+(** _Exercise 4_: Prove that the STλC is strongly normalizing.
    You can do this exercise even before doing any of the earlier exercises.
  *)
 Lemma strong_normalization (t : tm) (T : ty) : empty ⊢ t ∈ T -> halts t.
